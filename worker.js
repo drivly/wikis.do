9fd597034b9bf2472119be01dfc6628b640c1424
Nathan Clevenger <nateclev@gmail.com>
2022-09-03 09:46:31 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -14,7 +14,7 @@ const api = {
   endpoints: {
     data: 'https://wikis.do/:topic',
   },
-  type: 'https://apis.do/knowledge',
+  type: 'https://apis.do/data',
   repo: 'https://github.com/drivly/wikis.do',
 }
 
@@ -25,17 +25,17 @@ router.any('*', async (req, env, ctx) => {
 
 router.get('/:id', withParams, async ({id,user}) => {
   const doc = await wtf.fetch(decodeURI(id), 'en')
-  const data = doc?.json()
+  const json = doc?.json()
   const infoboxes = camelcaseKeys(data?.sections[0]?.infoboxes, { deep: true }) //doc.infoboxes()
   
-  const info = infoboxes?.map(infobox => Object.entries(infobox).reduce((obj,[key,val]) => ({ ...obj, [key]: val.links ? val.links.reduce((acc, val) => ({...acc, [val.text ?? val.page]: 'https://wikis.do/' + val.page}),{}) : val.text }),{}))
+  const data = infoboxes?.map(infobox => Object.entries(infobox).reduce((obj,[key,val]) => ({ ...obj, [key]: val.links ? val.links.reduce((acc, val) => ({...acc, [val.text ?? val.page]: 'https://wikis.do/' + val.page}),{}) : val.text }),{}))
     
   const links = doc?.links().reduce((acc,l) => ({...acc, [l.page()]: 'https://wikis.do/' + l.page()}),{})
   const text = doc?.text()
 //   const markdown = doc.markdown()
   const categories = doc?.categories().reduce((acc, val) => ({...acc, [val]: 'https://wikis.do/' + val}),{})
   
-  return json({api, info, categories, user, data, links})
+  return json({api, data, categories, links, user, })
 })
 
 export default {
