ef710082a1491b989ca12274e7a6e2989b61870e
Nathan Clevenger <nateclev@gmail.com>
2022-09-03 08:10:35 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,11 @@ import { Router } from 'itty-router'
 import { json } from 'itty-router-extras'
 import wtf from 'wtf_wikipedia'
 
-router.get('/', (req, env, ctx) => {
+router.any('*', async (req, env, ctx) => {
+  req.ctx = await this.env.CTX.fetch(req).then(res => res.json())
+}
+
+router.get('/', async (req, env, ctx) => {
   data = await wtf.fetch('Tony Hawk', 'en')
   return json({data})
 })
